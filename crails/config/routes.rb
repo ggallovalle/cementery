2dcb69bcae2402978f46b38ae589c86d2a7228be
Gerson Gallo <gerson.gallo@contollo.net>
2021-07-04 11:13:32 -0600

    chore(crails): write docs for rails routing

--- crails/config/routes.rb
+++ crails/config/routes.rb
@@ -1,3 +1,13 @@
+class Xampler
+  def call(env)
+    [ 200, { "Content-Type" => "application/json" }, [env.to_s]]
+  end
+end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/articles", to: "articles#index"
+  get "/articles" => "articles#index"
+  get "/adios" => -> (hash) { [200, {}, ["Comming Soon"]]}
+  get "/later" => Xampler.new
 end
