3db90cbe462582c997e91f2551c9217127d8b0ea
Gerson Gallo <ggallovalle@gmail.com>
2021-07-17 14:19:23 -0600

    feat(cpython): object identity

--- cpython/tests/test_cpython.py
+++ cpython/tests/test_cpython.py
@@ -3,3 +3,15 @@ from cpython import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_same_object():
+    class A:
+        pass
+    a = A()
+    a2 = A()
+
+    assert 1 is 1
+    assert 1 is not 2
+    assert A is A
+    assert a is a
+    assert a is not a2
